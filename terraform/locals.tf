006441480621870faa09d2c7b3f8bf9b47d5e65f
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2026-07-04 14:25:18 +0300

    [INFRA] Azure provider and terrafomr variables

---
+++ terraform/locals.tf
@@ -0,0 +1,8 @@
+locals {
+  common_tags = {
+    Project     = "Python DevSecOps Pipeline"
+    Environment = "Demo"
+    ManagedBy   = "Terraform"
+    Owner       = "Mohamed Abdelrazek"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "network_name" {
+  description = "Virtual Network name"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Subnet name"
+  type        = string
+}
+
+variable "network_security_group_name" {
+  description = "Network Security Group name"
+  type        = string
+}
+
+variable "public_ip_name" {
+  description = "Public IP name"
+  type        = string
+}
+
+variable "network_interface_name" {
+  description = "Network Interface name"
+  type        = string
+}
+
+variable "vm_name" {
+  description = "Virtual Machine name"
+  type        = string
+}
