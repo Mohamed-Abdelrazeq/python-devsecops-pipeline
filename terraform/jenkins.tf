e49ed2e3651a4a0ca6d5ab2ee53568818b424894
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2026-07-05 11:07:10 +0300

    [INFRA] jenkins.vm and it's networking

---
+++ terraform/jenkins.tf
@@ -0,0 +1,60 @@
+resource "azurerm_public_ip" "jenkins" {
+  name                = var.public_ip_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+
+  tags = local.common_tags
+}
+
+resource "azurerm_network_interface" "jenkins" {
+  name                = var.network_interface_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.jenkins.id
+  }
+
+  tags = local.common_tags
+}
+
+resource "azurerm_linux_virtual_machine" "jenkins" {
+  name                = var.vm_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  size                = "Standard_B1s"
+
+  admin_username = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.jenkins.id
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.public_key_path)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+
+  disable_password_authentication = true
+
+  tags = local.common_tags
+}
+
