c8e3a4e94a1e87220ee3ed6a8753d5582440b83c
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2026-07-07 09:57:13 +0300

    [INFRA] updated the security to be on NIC level not subnet

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,6 +1,6 @@
-resource "azurerm_network_security_group" "main" {
-
-  name                = var.network_security_group_name
+# Jenkins NSG
+resource "azurerm_network_security_group" "jenkins" {
+  name                = "jenkins-nsg"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
@@ -28,14 +28,60 @@ resource "azurerm_network_security_group" "main" {
     source_port_range      = "*"
     destination_port_range = "8080"
 
-    source_address_prefix      = "*"
+    source_address_prefix      = var.allowed_ssh_ip
     destination_address_prefix = "*"
   }
 
   tags = local.common_tags
 }
 
-resource "azurerm_subnet_network_security_group_association" "main" {
-  subnet_id                 = azurerm_subnet.main.id
-  network_security_group_id = azurerm_network_security_group.main.id
+# Flask NSG
+resource "azurerm_network_security_group" "flask" {
+  name                = "flask-nsg"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name      = "Allow-SSH"
+    priority  = 100
+    direction = "Inbound"
+    access    = "Allow"
+    protocol  = "Tcp"
+
+    source_port_range      = "*"
+    destination_port_range = "22"
+
+    source_address_prefix      = var.allowed_ssh_ip
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name      = "Allow-Flask"
+    priority  = 110
+    direction = "Inbound"
+    access    = "Allow"
+    protocol  = "Tcp"
+
+    source_port_range      = "*"
+    destination_port_range = "5000"
+
+    source_address_prefix      = var.allowed_ssh_ip
+    destination_address_prefix = "*"
+  }
+
+  tags = local.common_tags
+}
+
+#################################################
+# NSG Associations
+#################################################
+
+resource "azurerm_network_interface_security_group_association" "jenkins" {
+  network_interface_id      = azurerm_network_interface.jenkins.id
+  network_security_group_id = azurerm_network_security_group.jenkins.id
+}
+
+resource "azurerm_network_interface_security_group_association" "flask" {
+  network_interface_id      = azurerm_network_interface.flask_app.id
+  network_security_group_id = azurerm_network_security_group.flask.id
 }
